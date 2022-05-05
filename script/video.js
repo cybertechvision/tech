1031541cdcd873b7307dc2655bdcd5914ac28a83
cybertechvision <hellopoisoneye@gmail.com>
2022-05-05 18:41:58 +0530

    Add files via upload

---
+++ script/video.js
@@ -0,0 +1,36 @@
+$('.play-icon').click(function () {
+    var video = '<iframe allowfullscreen src="' + $(this).attr('data-video') + '"></iframe>';
+    $(this).replaceWith(video);
+});
+
+$('.play-icon').mousemove(function (e) {
+    var parentOffset = $(this).offset();
+    var relX = e.pageX - parentOffset.left;
+    var relY = e.pageY - parentOffset.top;
+    $(".play-button").css({ left: relX, top: relY});
+});
+$('.play-icon').mouseout(function() {
+    $(".play-button").css({ left: '50%', top: '50%'});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
